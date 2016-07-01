10c4aab9d43911af468472359ea5b99b161405d4
Stephen Bird <sbird@cyngn.com>
2016-06-30 17:07:07 -0700

    Fix versioning
    Change-Id: Icd9d30b90dc675a353644b157f4e1ff729d3f04f

--- swe/tools/swe_version.py
+++ swe/tools/swe_version.py
@@ -97,11 +97,11 @@ def get_build_id(directory):
   if hsh is None:
     return None
 
-  merge_base = getData(directory, ['merge-base', 'remotes/origin/master', '%s' %(hsh)])
+  merge_base = getData(directory, ['merge-base', 'remotes/origin/m52', '%s' %(hsh)])
   if merge_base is None:
     return None
 
-  patch_count = getData(directory, ['rev-list', '%s..' %(merge_base), '--count', '--author=codeaurora'])
+  patch_count = getData(directory, ['rev-list', '%s..' %(merge_base), '--count'])
   if patch_count is None:
     return None
 
@@ -150,11 +150,10 @@ def main():
   options = parser.parse_args()
 
   path = os.path.dirname(os.path.abspath(__file__))
-
   buildid = get_build_id(path)
   values = dict([('BUILDID', '%s' %(buildid))])
 
-  last_swe_change = getData(path, ['log', '-1', '--committer=@codeaurora.org', '--pretty=%H'] )
+  last_swe_change = getData(path, ['log', '-1', '--pretty=%H'] )
   values.update({'LASTSWECHANGE' :'%s' %(last_swe_change)})
 
   #fetch KEY=VALUE pairs
